6a5d403f2f4dac53f3594ae58658882eaba02953
Rifky Sultan Karisma Andriansyah <batuhc105@gmail.com>
2024-09-12 15:30:54 +0700

    fix: updates jwt to profile entity

--- src/modules/auth/use-cases/logout.use-case.ts
+++ src/modules/auth/use-cases/logout.use-case.ts
@@ -1,14 +1,14 @@
 import { AuthService } from 'src/services';
 import { Transactional } from '@nestjs-cls/transactional';
 import { Injectable } from '@nestjs/common';
-import { JwtEntity } from 'src/cores/entities';
+import { ProfileEntity } from 'src/cores/entities';
 
 @Injectable()
 export class LogoutUseCase {
   constructor(private readonly authService: AuthService) {}
 
   @Transactional()
-  async logout(payload: JwtEntity) {
-    return await this.authService.destroy(payload.sub);
+  async logout(payload: ProfileEntity) {
+    return await this.authService.destroy(payload.profile.id);
   }
 }
--- src/modules/auth/use-cases/refresh.use-case.ts
+++ src/modules/auth/use-cases/refresh.use-case.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { AuthService } from 'src/services';
 import { Transactional } from '@nestjs-cls/transactional';
-import { JwtEntity } from 'src/cores/entities';
+import { ProfileEntity } from 'src/cores/entities';
 
 @Injectable()
 export class RefreshUseCase {
   constructor(private readonly authService: AuthService) {}
 
   @Transactional()
-  async refresh(payload: JwtEntity) {
+  async refresh(payload: ProfileEntity) {
     const { user, permissions, token } = await this.authService.revalidateToken(
-      payload.sub,
+      payload.profile.id,
     );
 
     return {
